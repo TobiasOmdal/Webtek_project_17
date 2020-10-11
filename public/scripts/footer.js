b1e1081bf51873774541c745b4f6102032c4088e
sondresorbye0111 <sondre@magson.no>
2020-10-11 22:13:44 +0200

    moduler

---
+++ public/scripts/footer.js
@@ -0,0 +1,14 @@
+document.querySelector('footer').innerHTML = /*html*/`
+
+<h3>
+    <a href="index.html">SkiMentor</a>
+</h3>
+<span id="legalLinks">
+    <a href="terms.html" class="legalLink">Terms and Conditions</a>
+    <a href="privacy.html" class="legalLink">Privacy Policy</a>
+</span>
+<h4>Utviklet av
+    <a href="https://magson.no" target="_blank">MAGSON</a>
+</h4>
+
+`;
--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,53 @@
+document.querySelector("header").innerHTML = /*html*/`
+
+<div id="navBar">
+<a href="index.html" id="homeLink">
+    <img src="assets/logo.png" id="logo">
+    <h1>SkiMentor</h1>
+</a>
+<div id="horizontalNav">
+    <ul>
+        <li>
+            <a href="kurs.html" class="navBtn">Kurs</a>
+        </li>
+        <li>
+            <a href="trenere.html" class="navBtn">Trenere</a>
+        </li>
+        <li>
+            <a href="om-oss.html" class="navBtn">Om oss</a>
+        </li>
+        <li class="downloadBtn">
+            <a class="navBtn">Last ned</a>
+        </li>
+    </ul>
+</div>
+<svg id="menuBtn" height="22" viewBox="0 0 22 15" width="22">
+    <g>
+        <rect id="topHamburgerLine" height="5" rx="2.5" width="22" />
+        <rect id="bottomHamburgerLine" height="5" rx="2.5" width="22" y="10" />
+    </g>
+</svg>
+</div>
+<nav style="top: -300px;" id="dropDown">
+<ul>
+    <li>
+        <a href="kurs.html" class="navBtn">Kurs</a>
+    </li>
+    <li>
+        <a href="trenere.html" class="navBtn">Trenere</a>
+    </li>
+    <li>
+        <a href="om-oss.html" class="navBtn">Om oss</a>
+    </li>
+</ul>
+<div class="downloadBtn">
+    <a class="navBtn">Last ned</a>
+</div>
+</nav>
+
+`;
+
 var dropDownIsOpen = false;
-
-
 function animateHamburgerBtn(open) {
     const options = {
         duration: 200,
@@ -49,16 +96,17 @@ function toggleDropDown() {
     }
 }
 
+// ikke lukk menyen når du klikker i menyen 
+dropDown.onclick = (event) => {
+    console.log("hello")
+    event.stopPropagation();
+};
+
 // når du klikker utenfor menyen -> lukk
 window.onclick = () => {
     if (dropDownIsOpen) {
         toggleDropDown();
     }
-};
-
-// ikke lukk menyen når du klikker i menyen 
-dropDown.onclick = (event) => {
-    event.stopPropagation();
 };
 
 window.onscroll = () => {
