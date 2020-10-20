68ce2cc6484947244878c7ee524a3a0e3388830a
sondresorbye0111 <sondre@magson.no>
2020-10-20 21:25:55 +0200

    fiksing på trenere.html

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -4,8 +4,8 @@ document.querySelector('footer').innerHTML = /*html*/`
     <a href="index.html">SkiMentor</a>
 </h3>
 <span id="legalLinks">
-    <a href="terms.html" class="legalLink">Terms and Conditions</a>
-    <a href="privacy.html" class="legalLink">Privacy Policy</a>
+    <a href="https://app.termly.io/document/terms-of-use-for-website/36942f19-7743-4eeb-b206-f5aa33913543" class="legalLink">Terms and Conditions</a>
+    <a href="https://app.termly.io/document/privacy-notice/934f77b9-6959-4826-8455-35b46608a73b" class="legalLink">Privacy Policy</a>
 </span>
 <h4>Utviklet av
     <a href="https://magson.no" target="_blank">MAGSON</a>
