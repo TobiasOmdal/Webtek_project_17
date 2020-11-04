f7384f56ccd795e14c1fafd8a1e5568985ea30eb
sondresorbye0111 <sondre@magson.no>
2020-11-04 12:26:52 +0100

    fixes

--- public/scripts/downloadRedirect.js
+++ public/scripts/downloadRedirect.js
@@ -2,24 +2,13 @@ document.querySelectorAll('.downloadBtn').forEach(btn => btn.onclick = () => {
     // OS detection from: http://stackoverflow.com/a/9039885/177710
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
     if (/android/i.test(userAgent)) {
-        window.open("https://play.google.com/store/apps/details?id=com.Magson.SpaceRide");
+        window.open("https://play.google.com/store/apps/details?id=com.magson.SkiMentor");
     }
     else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        // TODO: add app store link when app launches
         window.open("https://itunes.apple.com/us/app/space-safari/id1215385382?l=nb&ls=1&mt=8");
     }
     else {
-        window.open("https://nrk.no");
+        window.open("index.html#badges");
     }
 });
-
-/* Kursside  */
-
-function visMer(e) {
-  e = e || window.event;
-  var target = e.target || e.srcElement;
-  var  cardTextEl = target.parentNode.querySelector(".text");
-  var dots = cardTextEl.querySelector(".dots");
-  var merTekst = cardTextEl.querySelector(".more");
-  $(merTekst).slideToggle("slow","swing");
-  $(dots).slideToggle("fast");
-};
